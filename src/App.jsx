f895c0866377032f156c0ac3463a60a3c06b67b4
molla mehedi <mollameehedi@gmail.com>
2024-01-12 08:48:09 +0600

    react router add

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider
+} from "react-router-dom";
+import Registration from "./pages/registration/Registration";
+import Login from "./pages/login/Login";
+
 function App() {
 
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Login/>} />
+        <Route path="/registration" element={<Registration/>} />
+      </>
+    )
+  );
   return (
     <>
-      <h3>hello world</h3>
+      <RouterProvider
+        router={router}
+      />
     </>
   )
 }
