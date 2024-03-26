5d28f4b3b962fd0e45b114b215d80ada4a767043
molla mehedi <mollameehedi@gmail.com>
2024-03-26 15:51:03 +0600

    message page almost design done

--- src/components/home/UserLIst.jsx
+++ src/components/home/UserLIst.jsx
@@ -1,5 +1,5 @@
-import React,{useEffect, useState} from 'react'
-import { getDatabase, ref, onValue,set,push,remove  } from "firebase/database";
+import React, { useEffect, useState } from 'react'
+import { getDatabase, ref, onValue, set, push, remove } from "firebase/database";
 import GroupCard from '../common/groupcard/GroupCard';
 import Image from '../../utils/Image'
 import { FaPlus } from "react-icons/fa";
@@ -8,117 +8,130 @@ import { toast } from 'react-toastify';
 
 
 const UserLIst = () => {
-    const db = getDatabase();
+  const db = getDatabase();
 
-    const [users, setUsers] = useState([]);
-    const[fRequest,setfRequest] = useState([]);
-    const[friendList,setFriendList] = useState([]);
-    const data = useSelector((state) => state.loginuserdata.value)
+  const [users, setUsers] = useState([]);
+  const [fRequest, setfRequest] = useState([]);
+  const [friendList, setFriendList] = useState([]);
+  const data = useSelector((state) => state.loginuserdata.value)
+  const [requestCheck, setRequestCheck] = useState([]);
 
 
-    // get users data
-   useEffect(() => {
+  // get users data
+  useEffect(() => {
     const userRef = ref(db, 'users/');
     onValue(userRef, (snapshot) => {
-        let arr = [];
-        snapshot.forEach((item) => {
-          if(data.uid != item.key){
-            arr.push({ ...item.val(), id: item.key });
-          }
-        });
-       setUsers(arr);
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (data.uid != item.key) {
+          arr.push({ ...item.val(), id: item.key });
+        }
+      });
+      setUsers(arr);
     });
-   },[])
+  }, [])
 
-   // get friends data
- useEffect(()=> {
-  const friendsRef = ref(db, 'friends');
-  onValue(friendsRef, (snapshot) => {
-    let arr = [];
-    snapshot.forEach((item) => {
-      if(item.val().whoreceiveid == data.uid || item.val().whosendid == data.uid){
-        arr.push(item.val().whoreceiveid + item.val().whosendid)
-      }
-    })
-    setFriendList(arr);
-  });
- },[])
+  // get friends data
+  useEffect(() => {
+    const friendsRef = ref(db, 'friends');
+    onValue(friendsRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (item.val().whoreceiveid == data.uid || item.val().whosendid == data.uid) {
+          arr.push(item.val().whoreceiveid + item.val().whosendid)
+        }
+      })
+      setFriendList(arr);
+    });
+  }, [])
 
-// get friend request data
-   useEffect(()=> {
+  // get friend request data
+  useEffect(() => {
     const fRequestRef = ref(db, 'friendrequest/');
     onValue(fRequestRef, (snapshot) => {
       let arr = [];
+      let xyz = [];
       snapshot.forEach((item) => {
-        if(data.uid == item.val().sender_id){
+        if (item.val().receiver_id == data.uid) {
+          xyz.push(item.val().sender_id + item.val().receiver_id)
+        }
+
+        if (data.uid == item.val().sender_id) {
           arr.push(item.val().sender_id + item.val().receiver_id)
         }
       })
       setfRequest(arr);
+      setRequestCheck(xyz)
     });
-   },[])
-   console.log(fRequest);
-   
-   // handle friend request
-   let handleFRequest = (requestinfo)=>{
-    set(ref(db, 'friendrequest/'+requestinfo.id),{
-      sender_id:data.uid,
-      sendername:data.displayName,
-      senderimg:data.photoURL,
-      senderemail:data.email,
-      receiver_id:requestinfo.id,
-      receivername:requestinfo.username,
-      receiverimg:requestinfo.profileimg,
-      receiveremail:requestinfo.email,
-  
-  }).then(() => {
-  toast.success('Friend Request Sent');
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-   }
+  }, [])
+  console.log(fRequest);
 
-   let handleCencelReqeust = (cencelInfo) =>{
-     remove(ref(db, 'friendrequest/' + cencelInfo.id)).then(() => {
+  // handle friend request
+  let handleFRequest = (requestinfo) => {
+    set(ref(db, 'friendrequest/' + requestinfo.id), {
+      sender_id: data.uid,
+      sendername: data.displayName,
+      senderimg: data.photoURL,
+      senderemail: data.email,
+      receiver_id: requestinfo.id,
+      receivername: requestinfo.username,
+      receiverimg: requestinfo.profileimg,
+      receiveremail: requestinfo.email,
+
+    }).then(() => {
+      toast.success('Friend Request Sent');
+    })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  let handleCencelReqeust = (cencelInfo) => {
+    remove(ref(db, 'friendrequest/' + cencelInfo.id)).then(() => {
       toast.error('Friend Request Canceled!!');
     })
-   }
+  }
 
   return (
     <>
-         <GroupCard cardTitle='Friend List'>
+      <GroupCard cardTitle='Friend List'>
         <div className="usermainbox">
-            {users && users.length > 0 ? users.map((user,index) => (
-             <div className='useritem' key={index}>
-             <div className='userimgbox'>
-             <Image src={user.profileimg} />
-             </div>
-             <div className='userinfobox'>
-             <div>
-                 <h3>{user.username}</h3>
-                 <p>Mern developer</p>
-             </div>
-             {fRequest.length > 0 && fRequest.includes(user.id + data.uid) || fRequest.includes( data.uid + user.id) ?
-                <>
-                <button className='addbutton'>Pending</button> 
-               <button className='addbutton' onClick={() => handleCencelReqeust(user)}>Cancel</button> 
-                </>
-               : 
-               friendList.includes(user.id + data.uid) || friendList.includes(data.uid+user.id) ?
-               
-               <button  className='addbutton text-danger' >Friends</button>
-               :
-               <button onClick={ () => handleFRequest(user)} className='addbutton text-danger' ><FaPlus /></button>
-               }
-             </div>
-             </div>
-               
-           ))
-          :
-          <p>User Not Found</p>
+          {users && users.length > 0 ? users.map((user, index) => (
+            <div className='useritem' key={index}>
+              <div className='userimgbox'>
+                <Image src={user.profileimg} />
+              </div>
+              <div className='userinfobox'>
+                <div>
+                  <h3>{user.username}</h3>
+                  <p>Mern developer</p>
+                </div>
+                {fRequest.length > 0 && fRequest.includes(user.id + data.uid) || fRequest.includes(data.uid + user.id) ?
+                  <>
+                    <button className='addbutton'>Pending</button>
+                    <button className='addbutton' onClick={() => handleCencelReqeust(user)}>Cancel</button>
+                  </>
+                  :
+                  friendList.includes(user.id + data.uid) || friendList.includes(data.uid + user.id) ?
+
+                    <button className='addbutton text-danger' >Friends</button>
+                    :
+                    requestCheck && requestCheck.includes(user.id + data.uid)
+                      ?
+                      <>
+                        <button onClick={() => handleCencelReqeust(user)} className='addbutton text-danger' >Confrim Request</button>
+                      </>
+                      :
+                      <button onClick={() => handleFRequest(user)} className='addbutton text-danger' ><FaPlus /></button>
+                }
+              </div>
+            </div>
+
+          ))
+            :
+            <p>User Not Found</p>
           }
-         
+
         </div>
       </GroupCard>
     </>
--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,8 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './message.css'
+import { useSelector, } from 'react-redux'
+import { toast } from 'react-toastify';
+import { getDatabase, ref, onValue, set, push, remove } from "firebase/database";
+import Image from '../../utils/Image';
 
 const Message = () => {
+
+  const db = getDatabase();
+  const [friendList, setFriendList] = useState([]);
+  const data = useSelector((state) => state.loginuserdata.value)
+
+  useEffect(() => {
+    const friendsRef = ref(db, 'friends');
+    onValue(friendsRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if (data.uid == item.val().whosendid || data.uid == item.val().whoreceiveid) {
+          arr.push({ ...item.val(), id: item.key })
+        }
+      })
+      setFriendList(arr);
+    });
+  }, [])
+
+
+  let handleUser = (i) => {
+    console.log(i);
+  }
   return (
-    <div>Message</div>
+    <div className='msg_wrapper'>
+      <div className='msg_user_body'>
+        <h3 className='list_heading'>Friend List</h3>
+        <div className="msg_user_wrapper">
+          {friendList && friendList.length > 0 ? friendList.map((item, index) => (
+            <div className='msg_user_item' key={index} onClick={ () =>handleUser(item)}>
+              <div className='userimgbox'>
+
+                <Image src={data.uid == item.whosendid ? item.whoreceivephoto : item.whosendphoto} />
+              </div>
+              <div className='userinfobox'>
+                <div>
+                  {data.uid == item.whosendid ? <h3>{item.whoreceivename}</h3> : <h3>{item.whosendname}</h3>}
+                  <p>Mern developer</p>
+                </div>
+                {/* <button className='addbutton' >Message</button> */}
+              </div>
+            </div>
+          )) :
+            <h3 >No Friend Available</h3>
+          }
+        </div>
+      </div>
+      <div className='msg_box_body'>
+        <div className="msg_box_heading">
+          <h2>Mehedi</h2>
+          <p>Active Now</p>
+        </div>
+        <div className="msg_main">
+        <div className="receivemsg">
+            <p>This is my Message</p>
+          </div>
+          <div className="sendmsg">
+            <p>hello</p>
+          </div>
+          <div className="receivemsg">
+            <p>This is my Message</p>
+          </div>
+          <div className="sendmsg">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quod sed voluptatibus atque labore, minus et dolorem repellat unde laboriosam culpa! Voluptatibus facilis eius placeat debitis temporibus, repudiandae illum illo?</p>
+          </div>
+        </div>
+        <div className="msg_footer">
+          <input type="text" placeholder='Please Enter Your Message' />
+        </div>
+      </div>
+    </div>
   )
 }
 
