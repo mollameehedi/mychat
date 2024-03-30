9a1aeb514750eb63ea8bd8bce46a245b8294a637
molla mehedi <mollameehedi@gmail.com>
2024-03-31 03:09:50 +0600

    message write and get

--- src/pages/message/Message.jsx
+++ src/pages/message/Message.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import './message.css'
-import { useSelector, } from 'react-redux'
+import { useSelector,useDispatch } from 'react-redux'
 import { toast } from 'react-toastify';
 import { getDatabase, ref, onValue, set, push, remove } from "firebase/database";
 import Image from '../../utils/Image';
+import { activeuser } from '../../slices/activeUserSlice';
 
 const Message = () => {
 
+  const [allmessage, setAllMessage] = useState([]);
+  const [msgtext, setMsgtext] = useState("");
   const db = getDatabase();
   const [friendList, setFriendList] = useState([]);
   const data = useSelector((state) => state.loginuserdata.value)
+  const dispatch = useDispatch()
+  const activechat = useSelector((state) => state.activeuserdata.value)
 
   useEffect(() => {
     const friendsRef = ref(db, 'friends');
@@ -25,9 +30,43 @@ const Message = () => {
   }, [])
 
 
+
   let handleUser = (i) => {
-    console.log(i);
+    dispatch(activeuser(i))
   }
+
+  // message write oparation
+  let handleSubmit = () => {
+    set(push(ref(db,'message')),{
+      senderid:data.uid,
+      senderemail:data.email,
+      sendenamed:data.displayName,
+      sendermsg:msgtext,
+      receiverid:data.uid == activechat.whoreceiveid ? activechat.whosendid : activechat.whoreceiveid,
+      receivername:data.uid == activechat.whoreceiveid ? activechat.whosendname : activechat.whoreceivename,
+      receiveremail:data.uid == activechat.whoreceiveid ? activechat.whosendemail : activechat.whoreceiveemail,
+    })
+    .then(() => {
+      console.log('success');
+    })
+    .catch((error) => {
+      
+    })
+  }
+
+  // message read oparation
+  useEffect(()=> {
+    const messageRef = ref(db, 'message');
+    onValue(messageRef, (snapshot) => {
+      let arr = [];
+      snapshot.forEach((item) => {
+        if(data.uid == item.val().senderid || data.uid == item.val().receiverid){
+          arr.push({...item.val(),id:item.key})
+        }
+      })
+      setAllMessage(arr);
+    });
+   },[activechat])
   return (
     <div className='msg_wrapper'>
       <div className='msg_user_body'>
@@ -52,13 +91,19 @@ const Message = () => {
           }
         </div>
       </div>
-      <div className='msg_box_body'>
+      {activechat != null ?
+        <div className='msg_box_body'>
         <div className="msg_box_heading">
-          <h2>Mehedi</h2>
+          <h2>{ activechat && activechat.whosendid == data.uid ? activechat.whoreceivename : activechat.whosendname }</h2>
           <p>Active Now</p>
         </div>
         <div className="msg_main">
-        <div className="receivemsg">
+          { allmessage.map((item,index) => (
+             <div className="receivemsg" key={index}>
+             <p>{item.sendermsg}</p>
+           </div>
+          ))}
+        {/* <div className="receivemsg">
             <p>This is my Message</p>
           </div>
           <div className="sendmsg">
@@ -69,12 +114,17 @@ const Message = () => {
           </div>
           <div className="sendmsg">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure quod sed voluptatibus atque labore, minus et dolorem repellat unde laboriosam culpa! Voluptatibus facilis eius placeat debitis temporibus, repudiandae illum illo?</p>
-          </div>
+          </div> */}
         </div>
         <div className="msg_footer">
-          <input type="text" placeholder='Please Enter Your Message' />
+          {/* <input onChange={handleForm} type="text" placeholder='Please Enter Your Message' className='msg_input'/> */}
+          <input onChange={(e) => setMsgtext(e.target.value)} type="text" placeholder='Please Enter Your Message' className='msg_input'/>
+          <button onClick={handleSubmit} className='mgs_send_btn'>Send</button>
         </div>
       </div>
+      :
+      <h1>Please Select a User</h1>
+      }
     </div>
   )
 }
